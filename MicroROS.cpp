63c8611ed254f9fd8148cbf7a8e1d9423b21b546
Shirish Jamthe <sjamthe@yahoo.com>
2025-01-16 21:45:12 -0800

    now publishing test /scan topic

--- MicroROS.cpp
+++ MicroROS.cpp
@@ -15,7 +15,7 @@
 #define ROS_NODE_NAME "esp32_node"
 #define ROS_NAMESPACE ""
 
-rcl_publisher_t publisher;
+rcl_publisher_t scan_publisher;
 std_msgs__msg__String msg;
 rclc_executor_t executor;
 rcl_allocator_t allocator;
@@ -24,38 +24,70 @@ rcl_node_t node;
 rcl_timer_t timer;
 
 static inline void set_microros_wifi_transport_only(char * agent_ip, uint agent_port) {
-    static struct micro_ros_agent_locator locator;
-    locator.address.fromString(agent_ip);
-    locator.port = agent_port;
+		static struct micro_ros_agent_locator locator;
+		locator.address.fromString(agent_ip);
+		locator.port = agent_port;
 
-    rmw_uros_set_custom_transport(
-        false,
-        (void *) &locator,
-        arduino_wifi_transport_open,
-        arduino_wifi_transport_close,
-        arduino_wifi_transport_write,
-        arduino_wifi_transport_read
-    );
+		rmw_uros_set_custom_transport(
+				false,
+				(void *) &locator,
+				arduino_wifi_transport_open,
+				arduino_wifi_transport_close,
+				arduino_wifi_transport_write,
+				arduino_wifi_transport_read
+		);
 }
 
 void setupMicroROS() {
-  logPrint(LOG_INFO, "Starting MicroROS on IP %s, port %d\n",ROS_AGENT_IP, ROS_AGENT_PORT);
-  // Set the agent IP and port
-  set_microros_wifi_transport_only(ROS_AGENT_IP, ROS_AGENT_PORT);
+	logPrint(LOG_INFO, "Starting MicroROS on IP %s, port %d",ROS_AGENT_IP, ROS_AGENT_PORT);
+	// Set the agent IP and port
+	set_microros_wifi_transport_only(ROS_AGENT_IP, ROS_AGENT_PORT);
 
-  allocator = rcl_get_default_allocator();
+	allocator = rcl_get_default_allocator();
 
-  // Initialize micro-ROS support
-  if(rclc_support_init(&support, 0, NULL, &allocator) != RCL_RET_OK) {
-      logPrint(LOG_ERROR, "Error initializing micro-ROS support. %s \n",rcutils_get_error_string().str);
-      return;
-  }
+	// Initialize micro-ROS support
+	if(rclc_support_init(&support, 0, NULL, &allocator) != RCL_RET_OK) {
+			logPrint(LOG_ERROR, "Error initializing micro-ROS support. %s",rcutils_get_error_string().str);
+			return;
+	}
+	logPrint(LOG_INFO, "MicroROS support started on IP %s, port %d",ROS_AGENT_IP, ROS_AGENT_PORT);
 
-  // Create node
-  if(rclc_node_init_default(&node, ROS_NODE_NAME, ROS_NAMESPACE, &support)) {
-      logPrint(LOG_ERROR, "Error creating node. %s \n",rcutils_get_error_string().str);
-      return;
-  }
+	// Create node
+	if(rclc_node_init_default(&node, ROS_NODE_NAME, ROS_NAMESPACE, &support)) {
+			logPrint(LOG_ERROR, "Error creating node. %s",rcutils_get_error_string().str);
+			return;
+	}
+	logPrint(LOG_INFO, "MicroROS Node %s started", ROS_NODE_NAME);
 
-  logPrint(LOG_INFO, "MicroROS started on IP %s, port %d\n",ROS_AGENT_IP, ROS_AGENT_PORT);
+    // Create publisher
+    rcl_ret_t retval = rclc_publisher_init_default(&scan_publisher, &node,
+    	ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+    	"scan");
+	if(retval != RCL_RET_OK) {
+		logPrint(LOG_ERROR, "Error creating scan publisher. %s",rcutils_get_error_string().str);
+		return;
+	}
+	logPrint(LOG_INFO, "MicroROS publishing /scan topic now.");
+}
+
+void cleanupMicroROS() {
+	// Clean up micro-ROS 
+	rcl_publisher_fini(&scan_publisher, &node);
+	rcl_node_fini(&node);
+	rclc_support_fini(&support);
+}
+
+void publishScanMessage() {
+	msg.data.data = (char *)"Hello from ESP32!";
+	msg.data.size = strlen(msg.data.data);
+	msg.data.capacity = strlen(msg.data.data);
+    
+	rcl_publish(&scan_publisher, &msg, NULL);
+
+}
+
+void handleMicroROS() {
+    // Publish message
+	publishScanMessage();
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 }
--- NetworkTask.cpp
+++ NetworkTask.cpp
@@ -6,102 +6,106 @@
 #include "Logger.h"
 
 extern void setupMicroROS();
+extern void cleanupMicroROS();
+extern void handleMicroROS();
 
 void syncTime() {
-  struct tm timeinfo;
-  char timeStringBuff[50];  // Make sure this is large enough for your format
+    struct tm timeinfo;
+    char timeStringBuff[50];  // Make sure this is large enough for your format
 
-  // Configure timezone and NTP
-  configTzTime("PST8PDT,M3.2.0,M11.1.0", "pool.ntp.org", "time.nist.gov");
-  
-  // Wait for sync
-  time_t now = time(nullptr);
-  int retry = 0;
-  while (now < 8 * 3600 * 2) {
-    logPrint(LOG_INFO, "Waiting for NTP time sync...");
-    delay(500);
-    now = time(nullptr);
-    if (++retry > 20) break;  // Timeout after 10 seconds
-  }
-  
-  // Get and print local time
-  if (getLocalTime(&timeinfo)) {
-    logPrint(LOG_INFO, "Local time: ");
-    strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S %Z", &timeinfo);
-    logPrint(LOG_INFO, "Current time: %s", timeStringBuff);  
-  } else {
-    logPrint(LOG_ERROR, "Failed to obtain time");
-  }
+    // Configure timezone and NTP
+    configTzTime("PST8PDT,M3.2.0,M11.1.0", "pool.ntp.org", "time.nist.gov");
+
+    // Wait for sync
+    time_t now = time(nullptr);
+    int retry = 0;
+    while (now < 8 * 3600 * 2) {
+        logPrint(LOG_INFO, "Waiting for NTP time sync...");
+        delay(500);
+        now = time(nullptr);
+        if (++retry > 20) break;  // Timeout after 10 seconds
+    }
+    
+    // Get and print local time
+    if (getLocalTime(&timeinfo)) {
+        logPrint(LOG_INFO, "Local time: ");
+        strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S %Z", &timeinfo);
+        logPrint(LOG_INFO, "Current time: %s", timeStringBuff);  
+    } else {
+        logPrint(LOG_ERROR, "Failed to obtain time");
+    }
 }
 
 void setupWifi() {
     
-  WiFi.mode(WIFI_STA);
-  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-  WiFi.setHostname(hostname); 
-  WiFi.setAutoReconnect(true);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    logPrint(LOG_WARN, "WiFi Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  syncTime(); // Initialize correct time for logging.
+    WiFi.mode(WIFI_STA);
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+    WiFi.setHostname(hostname); 
+    WiFi.setAutoReconnect(true);
+    
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        logPrint(LOG_WARN, "WiFi Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
+    syncTime(); // Initialize correct time for logging.
 
-  // Enable mDNS responder
-  if(!MDNS.begin(hostname)) {
-    logPrint(LOG_ERROR, "Error setting up MDNS responder!");
-  }
+    // Enable mDNS responder
+    if(!MDNS.begin(hostname)) {
+        logPrint(LOG_ERROR, "Error setting up MDNS responder!");
+    }
 
-  logPrint(LOG_INFO, "WiFi connected to: %s", String(ssid));
-  logPrint(LOG_INFO, "IP address: %s", WiFi.localIP().toString());
-  logPrint(LOG_INFO, "Hostname: %s", WiFi.getHostname());
+    logPrint(LOG_INFO, "WiFi connected to: %s", String(ssid));
+    logPrint(LOG_INFO, "IP address: %s", WiFi.localIP().toString());
+    logPrint(LOG_INFO, "Hostname: %s", WiFi.getHostname());
 }
 
-void shutdownGracefully() {
-  // close all open connections etc for OTA update.
-  logPrint(LOG_INFO,"Shutting down connections ...");
+void restart() {
+    // close all open connections etc for OTA update.
+    logPrint(LOG_INFO,"Shutting down connections ...");
+    cleaupMicroROS();
+
+    delay(1000);  // Give some time for the OTA response to be sent
+    ESP.restart(); // Programmatically trigger a reset
 }
 
 void setupOTA() {
-  // OTA Setup
+    // OTA Setup
     ArduinoOTA.onEnd([]() {
     logPrint(LOG_INFO, "OTA update complete. About to reboot.");
-    shutdownGracefully();
-    delay(1000);  // Give some time for the OTA response to be sent
-    ESP.restart(); // Programmatically trigger a reset
+    restart();
     });
 
-  // Other OTA event handlers
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else
-      type = "filesystem";
-    logPrint(LOG_INFO, "\nStart OTA updating %s", type);
-  });
+    // Other OTA event handlers
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+        else
+        type = "filesystem";
+        logPrint(LOG_INFO, "\nStart OTA updating %s", type);
+    });
 
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    logPrint(LOG_INFO, "Core %d - OTA update progress: %u%%\r",xPortGetCoreID(), (progress / (total / 100)));
-  });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        logPrint(LOG_INFO, "Core %d - OTA update progress: %u%%\r",xPortGetCoreID(), (progress / (total / 100)));
+    });
 
-  ArduinoOTA.onError([](ota_error_t error) {
+    ArduinoOTA.onError([](ota_error_t error) {
     logPrint(LOG_INFO, "Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) logPrint(LOG_ERROR, "ArduinoOTA Auth Failed");
     else if (error == OTA_BEGIN_ERROR) logPrint(LOG_ERROR, "ArduinoOTA Begin Failed");
     else if (error == OTA_CONNECT_ERROR) logPrint(LOG_ERROR, "ArduinoOTA Connect Failed");
     else if (error == OTA_RECEIVE_ERROR) logPrint(LOG_ERROR, "ArduinoOTA Receive Failed");
     else if (error == OTA_END_ERROR) logPrint(LOG_ERROR, "ArduinoOTA End Failed");
-  });
+    });
 
-//set password for OTA
-  ArduinoOTA.setPassword(otaPassword);
+    //set password for OTA
+    ArduinoOTA.setPassword(otaPassword);
 
-  ArduinoOTA.begin(); // default port 3232
-  logPrint(LOG_INFO, "OTA service started");
+    ArduinoOTA.begin(); // default port 3232
+    logPrint(LOG_INFO, "OTA service started");
 }
 
 void setupNetwork() {
@@ -121,8 +125,11 @@ void networkTask(void *parameter) {
     //uint8_t lidarBuffer[LIDAR_BUFFER_SIZE];
     
     while(1) {
+        delay(100); // run every 100ms
         // Handle OTA updates
         ArduinoOTA.handle();
+
+        handleMicroROS();
         
         // Handle web server
         //server.handleClient();
